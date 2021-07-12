fa05c3737c0f4e3e99553582d57f623c57b6db75
alexideavila <57275192+alexideavila@users.noreply.github.com>
2021-07-12 11:04:42 -0700

    using c plus plus now

---
+++ cplusplus/basic_calculator.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void messageOutput(int, float);
+
+int main(){
+    //A basic calculator
+    int option;
+    string message = "Choose two numbers to use: ";
+    float a,b;
+    float result;
+    cout << "This is a basic calculator, choose the options below to get started" << endl;
+    cout << "1 for addition, 2 for subtraction, 3 for division, 4 for multiplication"<< endl;
+    cin >> option;
+
+    if (option == 1){ //addition
+        cout << message;
+        cin >> a >> b;
+        result = a+b;
+        messageOutput(option, result);
+    }
+    if (option == 2){
+        cout << message;
+        cin >> a >> b;
+        result = a - b;
+        messageOutput(option, result);
+    }
+    if (option == 3){
+        cout << message;
+        cin >> a >> b;
+        result = a / b;
+        messageOutput(option, result);
+    }
+    if (option == 4){
+        cout << message;
+        cin >> a >> b;
+        result = a * b;
+        messageOutput(option, result);
+    }
+    else{
+        cout << "Invalid option!" << endl;
+    }
+    return 0;
+}
+
+void messageOutput(int x, float math){
+    if (x==1){
+        cout << "Your addition is: " << math << endl;
+    }
+    if (x==2){
+        cout << "Your difference is: " << math << endl;
+    }
+    if (x==3){
+        cout << "Your division is: " << math << endl;
+    }
+    if (x==4){
+        cout << "Your product is: " << math << endl;
+    }
+    else{
+        cout << "Error in processing function" << endl;
+    }
+}
