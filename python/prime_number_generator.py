bfa65678fc731e7a272e102774c12bbb7dfd6b0b
alexideavila <57275192+alexideavila@users.noreply.github.com>
2021-07-12 09:40:40 -0700

    using VScode on Win10

---
+++ python/prime_number_generator.py
@@ -0,0 +1,11 @@
+for n in range(2,11):
+    for x in range(2,n):
+        print(n, 'mod', x, 'equals', n % x)
+        if n % x == 0:
+            print(n,'equals',x,'*',n//x)
+            break
+    else:
+        #this will print a prime number
+        print(n, 'is a prime number')
+            
+    
