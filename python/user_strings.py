4127b5dad76bbefc4cbf62532b6d47c4d4ab2995
alexideavila <57275192+alexideavila@users.noreply.github.com>
2021-07-15 09:12:28 -0700

    more comments

--- python/user_strings.py
+++ python/user_strings.py
@@ -12,21 +12,23 @@
 def spaceReader(x,y):
     #x is the string
     #y is the num of spaces
-    for a in range(0,(len(x)-1)):
-        if " " in x[a]:
-            y=y+1
+    for a in range(0,(len(x)-1)): #proper indexing because it arrays start at zero
+        if " " in x[a]: #looks for spaces
+            y=y+1 #the space counter
         else:
-            continue
-    return y
+            continue #if no spaces, the loop continues
+    return y #the full count being returned
 
-spaceNum=0
+spaceNum=0 #declaring the variable
 
 print("This program will take a message and measure it")
 output = input("Write a message: ") #python is nice enough to declare it as a string for you
 
 print("Your message is:", output)
-print("It is", len(output), "characters long")
+print("It is", len(output), "characters long") #len() reads the string length
 
+#Now I call the function which will return the num of spaces
+#and I can do it in once single instance, thanks to Python
 
-print("And there was", spaceReader(output, spaceNum), "spaces") #prints out the number of spaces
+print("And there was", spaceReader(output, spaceNum), "spaces")
 print("See ya!")
