92a496511008ba7ab9cade45d500291c95ca644d
alexideavila <57275192+alexideavila@users.noreply.github.com>
2021-07-15 08:49:20 -0700

    more python stuff

---
+++ python/user_strings.py
@@ -0,0 +1,12 @@
+#------------------------------------------
+#   This a program that will read a user 
+#   input and give it back and measure how 
+#   long it was
+#------------------------------------------
+
+print("This program will take a message and measure it")
+output = input("Write a message: ") #python is nice enough to declare it as a string for you
+
+print("Your message is:", output)
+print("And it is", len(output), "characters long")
+print("See ya!")
