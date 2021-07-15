cbcaaba2d9ef02d5cc6ff4fc0174530cf8c39f82
alexideavila <57275192+alexideavila@users.noreply.github.com>
2021-07-15 09:03:07 -0700

    formatting and does more stuff

--- python/user_strings.py
+++ python/user_strings.py
@@ -1,12 +1,27 @@
-#------------------------------------------
-#   This a program that will read a user 
-#   input and give it back and measure how 
-#   long it was
-#------------------------------------------
+#---------------------------------------------#
+#   This a program that will read a user      #
+#   input and give it back and measure how    #
+#   long it was and how many spaces were      # 
+#   there                                     # 
+#---------------------------------------------#
+
+spaceNum=0
 
 print("This program will take a message and measure it")
 output = input("Write a message: ") #python is nice enough to declare it as a string for you
 
 print("Your message is:", output)
-print("And it is", len(output), "characters long")
+print("It is", len(output), "characters long")
+
+#this for loop reads the string as an array
+#cycles through looking spaces
+#and then prints how many there was
+
+for x in range(0,(len(output)-1)):
+    if " " in output[x]:
+        spaceNum=spaceNum+1
+    else:
+        continue
+
+print("And there was", spaceNum, "spaces") #prints out the number of spaces
 print("See ya!")
