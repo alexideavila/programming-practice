8e266253b277bff893666dbed273486de79a424f
alexideavila <57275192+alexideavila@users.noreply.github.com>
2021-07-15 09:58:06 -0700

    cleaned up and comments

--- conly/first_program.c
+++ conly/first_program.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#define MAXARRAY 100
-
+#define MAXARRAY 100 //not the best but will do
 
 int main(){
-    int x=0;
-    char userInput[MAXARRAY];
+    int x=0; //actual array counter
+    char userInput[MAXARRAY]; //declare a dummy size for an array
     
     printf("This is program that will read a string, print it and say how long it was\n");
     printf("Please write a message and it end it with a period: ");
@@ -12,20 +11,22 @@ int main(){
     //this while loop will read in the input of user
     //then x will take the actual count of the array
     //the condition is that it is a char
-    while(scanf("%c", &userInput[x])&&userInput[x]!='.'){ //use a period to end the while loop
+    while(scanf("%c", &userInput[x]) && userInput[x]!='.'){ //use a period to end the while loop
         x++;
     }
 
     //now we have to compensate for the period
     x--;
+    
+    printf("Your message is: ");
 
     //this will print our char array out
-    printf("Your message is: ");
     for(int i=0; i<=x; i++){
         printf("%c",userInput[i]);
     }
     printf("\n");
     printf("It has %d letters\n", x);
+    printf("See ya!");
 
     return 0;
 }
